ab4a45668e6210f0357374a24f297d64d882f59d
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-09-11 16:39:56 +0300

    Add files via upload

---
+++ 2sem/c++ Lab4/MyVectorLib(VS Studio)/MyVectorLib(VS Studio)/MyVectorLib(VS Studio).cpp
@@ -0,0 +1,10 @@
+﻿// MyVectorLib(VS Studio).cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnMyVectorLibVSStudio()
+{
+}
